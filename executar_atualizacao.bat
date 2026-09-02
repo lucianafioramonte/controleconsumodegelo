2787a903f007e82af377b9bb86c178c70c3ec2ab
lucianafioramonte <luciana.aparecidafio@gmail.com>
2026-09-02 11:05:16 -0400

    Delete executar_atualizacao.bat

--- executar_atualizacao.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Atualiza o painel Torre de Controle - Consumo de Gelo
-REM Coloque este .bat na mesma pasta do atualizar_painel_gelo.py e do template.html
-cd /d "%~dp0"
-python atualizar_painel_gelo.py >> log_atualizacao.txt 2>&1
